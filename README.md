a96581eabdce7d2b2cc73d1dd0abf44f4a136793
Simon Bengtsson <simben.02@gmail.com>
2024-03-05 10:44:58 +0100

    test

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
-# LP3 Drone Project - Lab 3
+# LP3 Drone Project - Lab 2
+Intall the requied Python packages, redis is added in the list
+```
+pip3 install -r requirements.txt
+```
 
-In this lab you will continue with your own code made for Lab2. But you need to replace the old `index.html` file with the one provided in `/templates/index.html` of this repo.
+## On the Server Pi:
+Go to `/webserver`, start your Redis server and run the two flask servers:
+1. On Terminal 1, start your redis server
+2. On Terminal 2, run `database.py`
+```
+export FLASK_APP=database.py
+export FLASK_ENV=development
+flask run --port=5001 --host 0.0.0.0
+```
+3. On Terminal 3, run `build.py`
+```
+export FLASK_APP=build.py
+export FLASK_ENV=development
+flask run --host 0.0.0.0
+```
+4. On Terminal 4, run `route_planner.py`
+```
+export FLASK_APP=route_planner.py
+export FLASK_ENV=development
+flask run --port=5002 --host 0.0.0.0
+```
 
+## On the Drone Pis:
+You need to install the Python packages in the requirements if you havn't done any. 
 
-## Optional
-A new `simulator.py` file is provided in `/pi`, which is cleaner and easier to understand (perhaps). You can use your own old `simulator.py` from Lab2 or replace it with the new one. If you want to use the new file, remember to put the changes you made on your own for Lab2 in this new file as well.
+Go to `/pi`, run `drone.py`
+```
+export FLASK_APP=drone.py
+export FLASK_ENV=development
+flask run --host 0.0.0.0
+```
 
-## /pygame-music
-In this lab we will need to play some sounds in the background while the drone is in different status. `pygame` is a new Python package we will use to play music. Take a look at the document in this folder and try to integarte it in `simulator.py`.
+Note: Don't user `python3 build.py` to run the servers, since this does not porvide all the functionalities requied by the application.
+
--- pi/simulator.py
+++ pi/simulator.py
@@ -2,8 +2,13 @@ import math
 import requests
 import argparse
 
+from sense_hat import sense_hat
+import pygame
+sense = SenseHat()
+
 def getMovement(src, dst):
-    speed = 0.00001
+
+    speed = 0.00003
     dst_x, dst_y = dst
     x, y = src
     direction = math.sqrt((dst_x - x)**2 + (dst_y - y)**2)
@@ -18,6 +23,13 @@ def moveDrone(src, d_long, d_la):
     return (x, y)
 
 def send_location(SERVER_URL, id, drone_coords, status):
+    if(status == 'idle'):
+        sense.show_letter("I", (0,255,0))
+    elif(status == 'busy'):
+        sense.show_letter("B", (255,0,0))
+    elif(status == 'waiting'):
+        sense.show_letter("W", (255,230,30))
+
     with requests.Session() as session:
         drone_info = {'id': id,
                       'longitude': drone_coords[0],
@@ -29,31 +41,49 @@ def send_location(SERVER_URL, id, drone_coords, status):
 def distance(_fr, _to):
     _dist = ((_to[0] - _fr[0])**2 + (_to[1] - _fr[1])**2)*10**6
     return _dist
-        
+
 def run(id, current_coords, from_coords, to_coords, SERVER_URL):
     drone_coords = current_coords
 
-    # Move from current_coodrs to from_coords
-    d_long, d_la =  getMovement(drone_coords, from_coords)
-    while distance(drone_coords, from_coords) > 0.0002:
-        drone_coords = moveDrone(drone_coords, d_long, d_la)
-        send_location(SERVER_URL, id=id, drone_coords=drone_coords, status='busy')
+    pygame.mixer.music.load()
 
-    # Move from from_coodrs to to_coords
-    d_long, d_la =  getMovement(drone_coords, to_coords)
-    while distance(drone_coords, to_coords) > 0.0002:
+    d_long, d_la =  getMovement(drone_coords, from_coords)
+    while ((from_coords[0] - drone_coords[0])**2 + (from_coords[1] - drone_coords[1])**2)*10**6 > 0.0002:
         drone_coords = moveDrone(drone_coords, d_long, d_la)
-        send_location(SERVER_URL, id=id, drone_coords=drone_coords, status='busy')
-    
-    # Stop and update status to database
-    send_location(SERVER_URL, id=id, drone_coords=drone_coords, status='idle')
-    
+        with requests.Session() as session:
+            drone_info = {'id': id,
+                          'longitude': drone_coords[0],
+                          'latitude': drone_coords[1],
+                          'status': 'busy'
+                        }
+            resp = session.post(SERVER_URL, json=drone_info)
+    d_long, d_la =  getMovement(drone_coords, to_coords)
+    while ((to_coords[0] - drone_coords[0])**2 + (to_coords[1] - drone_coords[1])**2)*10**6 > 0.0002:
+        drone_coords = moveDrone(drone_coords, d_long, d_la)
+        with requests.Session() as session:
+            drone_info = {'id': id,
+                          'longitude': drone_coords[0],
+                          'latitude': drone_coords[1],
+                          'status': 'busy'
+                        }
+            resp = session.post(SERVER_URL, json=drone_info)
+    with requests.Session() as session:
+            drone_info = {'id': id,
+                          'longitude': drone_coords[0],
+                          'latitude': drone_coords[1],
+                          'status': 'idle'
+                         }
+            resp = session.post(SERVER_URL, json=drone_info)
+    with open('coords.txt', 'w') as file:
+        file.write(str(drone_coords[0]) + '\n')
+        file.write(str(drone_coords[1]) + '\n')
+
     return drone_coords[0], drone_coords[1]
    
 if __name__ == "__main__":
     # Fill in the IP address of server, in order to location of the drone to the SERVER
     #===================================================================
-    SERVER_URL = "http://SERVER_IP:PORT/drone"
+    SERVER_URL = "http://192.168.1.1:5001/drone"
     #===================================================================
 
     parser = argparse.ArgumentParser()
@@ -65,13 +95,13 @@ if __name__ == "__main__":
     parser.add_argument("--tlat", help ='latitude of input [to address]' ,type=float)
     parser.add_argument("--id", help ='drones ID' ,type=str)
     args = parser.parse_args()
-
+    print(args.clong)
     current_coords = (args.clong, args.clat)
     from_coords = (args.flong, args.flat)
     to_coords = (args.tlong, args.tlat)
 
-    print("Get New Task!")
-
+    print(current_coords, from_coords, to_coords)
     drone_long, drone_lat = run(args.id ,current_coords, from_coords, to_coords, SERVER_URL)
     # drone_long and drone_lat is the final location when drlivery is completed, find a way save the value, and use it for the initial coordinates of next delivery
     #=============================================================================
+    
