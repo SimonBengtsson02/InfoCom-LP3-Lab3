0e88b0eef43f00f8b48c766a3c78767c44378ebb
Simon Bengtsson <simben.02@gmail.com>
2024-03-05 11:17:24 +0100

    l

--- pi/simulator.py
+++ pi/simulator.py
@@ -79,6 +79,7 @@ def run(id, current_coords, from_coords, to_coords, SERVER_URL):
 
     while ((to_coords[0] - drone_coords[0])**2 + (to_coords[1] - drone_coords[1])**2)*10**6 > 0.0002:
         drone_coords = moveDrone(drone_coords, d_long, d_la)
+        send_location(SERVER_URL, id=id, drone_coords=drone_coords, status='waiting')
         with requests.Session() as session:
             drone_info = {'id': id,
                           'longitude': drone_coords[0],
