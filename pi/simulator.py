6f89bbbd3c0bf974eaa4278cb6a242522da72f32
Simon Bengtsson <simben.02@gmail.com>
2024-03-05 10:51:08 +0100

    hoppas det funkar

--- pi/simulator.py
+++ pi/simulator.py
@@ -45,7 +45,8 @@ def distance(_fr, _to):
 def run(id, current_coords, from_coords, to_coords, SERVER_URL):
     drone_coords = current_coords
 
-    pygame.mixer.music.load()
+    pygame.mixer.music.load("space-odyssey.mp3")
+    pygame.mixer.music.play()
 
     d_long, d_la =  getMovement(drone_coords, from_coords)
     while ((from_coords[0] - drone_coords[0])**2 + (from_coords[1] - drone_coords[1])**2)*10**6 > 0.0002:
@@ -58,6 +59,21 @@ def run(id, current_coords, from_coords, to_coords, SERVER_URL):
                         }
             resp = session.post(SERVER_URL, json=drone_info)
     d_long, d_la =  getMovement(drone_coords, to_coords)
+
+    pygame.mixer.music.stop()
+    doorbell = pygame.mixer.Sound("doorbell-1.wav")
+    doorbell.play()
+
+    loop = True
+    while(loop):
+        for event in sense.stick.get_events():
+            print(event.action)
+            if event.action == "pressed" and event.direction == "up":
+                loop = False
+    
+    pygame.mixer.music.load("space-odyssey.mp3")
+    pygame.mixer.music.play()
+
     while ((to_coords[0] - drone_coords[0])**2 + (to_coords[1] - drone_coords[1])**2)*10**6 > 0.0002:
         drone_coords = moveDrone(drone_coords, d_long, d_la)
         with requests.Session() as session:
@@ -78,9 +94,17 @@ def run(id, current_coords, from_coords, to_coords, SERVER_URL):
         file.write(str(drone_coords[0]) + '\n')
         file.write(str(drone_coords[1]) + '\n')
 
+    pygame.mixer.music.stop()
+    doorbell.play()
+
     return drone_coords[0], drone_coords[1]
    
 if __name__ == "__main__":
+
+    pygame.mixer.init()
+    coin = pygame.mixer.Sound("coin.wav")
+    coin.play()
+
     # Fill in the IP address of server, in order to location of the drone to the SERVER
     #===================================================================
     SERVER_URL = "http://192.168.1.1:5001/drone"
